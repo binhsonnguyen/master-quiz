7b20f9b7ab6e748df05c4142960f08c0dc9699a6
binhsonnguyen <son@binhsonnguyen.com>
2018-09-13 16:03:01 +0700

    add a simple mvc configuration

---
+++ src/main/java/mst/codegym/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package mst.codegym.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{MvcWebApplicationInitializer.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MvcWebApplicationConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
