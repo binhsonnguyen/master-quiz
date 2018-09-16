1c75ee8b5b9989d624774a2a5fc2036183c281ee
binhsonnguyen <son@binhsonnguyen.com>
2018-09-16 10:58:38 +0700

    binding testquestion model

--- src/main/java/mst/codegym/controller/TestQuestionController.java
+++ src/main/java/mst/codegym/controller/TestQuestionController.java
@@ -1,7 +1,9 @@
 package mst.codegym.controller;
 
+import mst.codegym.model.TestQuestion;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,9 +11,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/question")
 public class TestQuestionController {
 
-    @GetMapping("")
+    @GetMapping
     public ModelAndView createQuizQuestion() {
         ModelAndView modelAndView = new ModelAndView("question/create");
+        TestQuestion question = new TestQuestion();
+        question.setContent("a sample content");
+        modelAndView.addObject("question", question);
         return modelAndView;
     }
+
 }
