63edbccf87f058e3845ce6066fee879d3c5897d3
binhsonnguyen <son@binhsonnguyen.com>
2018-09-16 15:52:04 +0700

    add TestQuestion repository

---
+++ src/main/java/mst/codegym/repository/TestQuestionRepository.java
@@ -0,0 +1,8 @@
+package mst.codegym.repository;
+
+import mst.codegym.model.TestQuestion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestQuestionRepository extends CrudRepository<TestQuestion, Long> {
+
+}
