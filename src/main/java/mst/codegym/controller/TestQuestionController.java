df3970b5414ef9678500af852f0610f22f1c6e5b
binhsonnguyen <son@binhsonnguyen.com>
2018-09-16 11:34:44 +0700

    serving add test question request

--- src/main/java/mst/codegym/controller/TestQuestionController.java
+++ src/main/java/mst/codegym/controller/TestQuestionController.java
@@ -1,21 +1,35 @@
 package mst.codegym.controller;
 
 import mst.codegym.model.TestQuestion;
+import mst.codegym.service.TestQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/question")
 public class TestQuestionController {
+    @Autowired
+    private TestQuestionService testQuestionService;
 
     @GetMapping
-    public ModelAndView createQuizQuestion() {
+    public ModelAndView accessCreateQuizQuestionPage() {
         ModelAndView modelAndView = new ModelAndView("question/create");
         TestQuestion question = new TestQuestion();
         question.setContent("a sample content");
         modelAndView.addObject("question", question);
+        return modelAndView;
+    }
+
+    @PostMapping
+    public ModelAndView createQuizQuestion(@ModelAttribute("question") TestQuestion question) {
+        testQuestionService.save(question);
+        ModelAndView modelAndView = new ModelAndView("question/details");
+        modelAndView.addObject("questioning", question);
         return modelAndView;
     }
 
