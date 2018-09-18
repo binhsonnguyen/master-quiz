8f38bf96a35092f57414cb391f5f5864e5c93f35
binhsonnguyen <son@binhsonnguyen.com>
2018-09-18 16:41:17 +0700

    create a dummy unittest

---
+++ src/test/java/mst/codegym/controller/TestQuestionControllerTest.java
@@ -0,0 +1,12 @@
+package mst.codegym.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestQuestionControllerTest {
+    @Test
+    public void accessCreateTestQuestionPage() {
+        assertEquals(1, 1);
+    }
+}
