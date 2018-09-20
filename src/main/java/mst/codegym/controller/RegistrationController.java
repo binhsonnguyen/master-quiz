e32852a3aae815ad05137995750a189b0fa52bf6
binhsonnguyen <son@binhsonnguyen.com>
2018-09-20 17:16:29 +0700

    `/register` url now has its endpoint

--- src/main/java/mst/codegym/controller/RegistrationController.java
+++ src/main/java/mst/codegym/controller/RegistrationController.java
@@ -1,7 +1,15 @@
 package mst.codegym.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/register")
 public class RegistrationController {
+    @GetMapping
+    public ModelAndView accessRegisterPage() {
+        return new ModelAndView("index");
+    }
 }
