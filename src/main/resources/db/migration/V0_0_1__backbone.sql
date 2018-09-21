281eef72c76d1d9511ef30174db0793ad1ca3e03
binhsonnguyen <son@binhsonnguyen.com>
2018-09-22 05:27:12 +0700

    update database migration scripts

--- src/main/resources/db/migration/V0_0_1__backbone.sql
+++ src/main/resources/db/migration/V0_0_1__backbone.sql
@@ -1,20 +1,21 @@
 CREATE TABLE Test
 (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
-  descriptions varchar(255),
-  name varchar(255) NOT NULL
+  id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+  descriptions VARCHAR(255),
+  name         VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE TestQuestion
 (
-  id bigint PRIMARY KEY AUTO_INCREMENT,
-  content varchar(255) NOT NULL,
-  descriptions varchar(255),
-  hint varchar(255),
-  orderRelevance bit DEFAULT false ,
-  weight int,
-  testId bigint,
+  id             BIGINT PRIMARY KEY AUTO_INCREMENT,
+  content        VARCHAR(255) NOT NULL,
+  descriptions   VARCHAR(255),
+  hint           VARCHAR(255),
+  orderRelevance BIT                DEFAULT FALSE,
+  weight         INT,
+  testId         BIGINT,
   CONSTRAINT TestQuestion_Test_id_fk FOREIGN KEY (testId) REFERENCES Test (id)
 );
 
-CREATE INDEX TestQuestion_testId_index ON TestQuestion (testId);
+CREATE INDEX TestQuestion_testId_index
+  ON TestQuestion (testId);
