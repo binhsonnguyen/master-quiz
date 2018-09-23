041b8a2461fa17dd12da76aca500d6d32545f9b6
binhsonnguyen <son@binhsonnguyen.com>
2018-09-23 21:35:10 +0700

    use factory to create test executor's instance

--- src/test/java/mst/codegym/web/view/thymeleaf/processor/MarkDownProcessorTest.java
+++ src/test/java/mst/codegym/web/view/thymeleaf/processor/MarkDownProcessorTest.java
@@ -1,21 +1,23 @@
 package mst.codegym.web.view.thymeleaf.processor;
 
 import mst.codegym.web.view.thymeleaf.dialect.MarkdownDialect;
+import mst.codegym.web.view.thymeleaf.util.TestExecutorFactory;
 import org.junit.jupiter.api.Test;
+import org.thymeleaf.dialect.IProcessorDialect;
 import org.thymeleaf.standard.StandardDialect;
 import org.thymeleaf.testing.templateengine.engine.TestExecutor;
-import org.thymeleaf.testing.templateengine.report.ConsoleTestReporter;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MarkDownProcessorTest {
     @Test
     void testMarkDownProcessor() {
-        final TestExecutor executor = new TestExecutor();
-        executor.setDialects(Arrays.asList(new StandardDialect(), new MarkdownDialect()));
-        executor.setReporter(new ConsoleTestReporter());
+        final List<IProcessorDialect> neededDialects =
+                Arrays.asList(new StandardDialect(), new MarkdownDialect());
+        final TestExecutor executor = TestExecutorFactory.createInstance(neededDialects);
         executor.execute("classpath:thymeleaf-tests/markdown.thtest");
         assertTrue(executor.isAllOK());
 
